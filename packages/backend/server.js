6035dac56b784cf8735d1296d24885a2df97c242
Vicky Rai <vraimondo@frba.utn.edu.ar>
2025-10-14 22:25:09 -0300

    Comenzamos swagger

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,29 +1,30 @@
 import express from "express";
 import logger from "../logger/logger.js";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 // El server recibe las rutas y recibe el puerto
 export default class Server {
-  #controllers = {};
-  #app;
-  #routes;
-
   constructor(app, port) {
-    this.#app = app;
+    this.app = app;
     this.port = port;
-    this.#routes = [];
-    this.#app.use(express.json());
+    this.routes = [];
+    this.controllers = {};
+    this.app.use(express.json());
+    const swaggerDocument = YAML.load(new URL("./swagger.yaml", import.meta.url));
+    this.app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   }
 
   app() {
-    
-    return this.#app;
+    return this.app;
   }
 
+
   setController(controllerClass, controller) {
-    this.#controllers[controllerClass.name] = controller;
+    this.controllers[controllerClass.name] = controller;
   }
 
   getController(controllerClass) {
-    const controller = this.#controllers[controllerClass.name];
+    const controller = this.controllers[controllerClass.name];
     if (!controller) {
       throw new Error("Controller missing for the given route.");
     }
@@ -31,17 +32,17 @@ export default class Server {
   }
 
   addRoute(route) {
-    this.#routes.push(route);
+    this.routes.push(route);
   }
 
   configureRoutes() {
-    this.#routes.forEach((route) =>
-      this.#app.use(route(this.getController.bind(this))),
+    this.routes.forEach((route) =>
+      this.app.use(route(this.getController.bind(this))),
     );
   }
 
   launch() {
-    this.#app.listen(this.port, () => {
+    this.app.listen(this.port, () => {
       logger.info("Server running on port " + this.port);
     });
   }
